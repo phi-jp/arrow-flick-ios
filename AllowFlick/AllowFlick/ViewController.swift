8d0ff90a5b441dd322efbd988c87ffa24764eafd
phi <phi1618jp@gmail.com>
2015-01-27 20:06:29 +0900

    first commit

---
+++ AllowFlick/AllowFlick/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AllowFlick
+//
+//  Created by Yuki Uematsu on 2015/01/27.
+//  Copyright (c) 2015年 phi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
